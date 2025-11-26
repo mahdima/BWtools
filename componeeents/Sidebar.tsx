52abde25f248f7eaa14ad9e1caca7b914f2a6a38
Mahdi Pc <badachemahdi17@gmail.com>
2025-11-26 01:07:52 +0100

    new side 2

--- componeeents/Sidebar.tsx
+++ componeeents/Sidebar.tsx
@@ -1,8 +1,46 @@
+import React from "react";
+import {
+  LayoutDashboard,
+  LucideIcon,
+  ShoppingCart,
+  ShieldUser,
+} from "lucide-react";
+import path from "path";
+import Sidebaritem from "./item";
+
+interface ISidebarItem {
+  name: string;
+  path: string;
+  icon: LucideIcon;
+}
+
+const items: ISidebarItem[] = [
+  {
+    name: "Dashboard",
+    path: "/",
+    icon: LayoutDashboard,
+  },
+  {
+    name: "Product",
+    path: "/product",
+    icon: ShoppingCart,
+  },
+  {
+    name: "Customers",
+    path: "/customers",
+    icon: ShieldUser,
+  },
+];
+
 const Sidebar = () => {
   return (
-    <>
-      <div className="w-[20%] p-4">Sidebar</div>
-    </>
+    <div className="w-[18%] ">
+      <div className="flex flex-col space-y-12 m-19 ">
+        {items.map((item) => (
+          <Sidebaritem key={item.path} item={item} />
+        ))}
+      </div>
+    </div>
   );
 };
 
